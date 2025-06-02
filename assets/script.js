0972f4dd9b0a5bb595e5f424f4258a48079cc07d
rachael444 <nnakwuchiamaka@gmail.c0m>
2025-06-02 09:54:04 +0000

    changes made on responsiveness

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// Declare variable to store navbar element
+const menuBtn = document.getElementById('menu-btn');
+// detect scroll event on the document
+menuBtn.addEventListener('click', function () {
+   alert('Hello')
+});
